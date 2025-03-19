294685a7ddabcd2f18276c97fdc5611376057ff5
bulatpython <bulatpython@gmail.com>
2025-03-19 12:15:47 +0300

    Сайт с рабочей формой добавления знаменитостей

---
+++ star/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='star_index'),  # Главная страница
+    path('person/<slug:slug>/', views.star_detail, name='star_detail'),  # Детальная страница
+
+    # Страница «О сайте»
+    path('about/', views.about, name='about'),
+
+    # Знаменитости по стране
+    path('country/<slug:slug>/', views.stars_by_country, name='stars_by_country'),
+
+    # Знаменитости по виду деятельности
+    path('industry/<slug:slug>/', views.stars_by_category, name='stars_by_category'),
+    path('add/', views.add_star, name='add_star'),  # Добавление знаменитости
+]
