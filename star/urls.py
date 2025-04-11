976fce0a0d9f847d5bf4683e7b789987bde099ec
itrucontent <kaistudents@yandex.ru>
2025-04-11 17:40:48 +0300

    Переехали на postgresql (без большого файла базы данных)

--- star/urls.py
+++ star/urls.py
@@ -1,20 +1,34 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from . import views
 
+# Время кэширования
+DAY = 60 * 60 * 24  # 24 часа
+WEEK = DAY * 7  # 7 дней
+
 urlpatterns = [
+    # Главная страница - кэшируем на день, но очищаем в полночь
     path('', views.index, name='star_index'),
+
+    # Детальные страницы знаменитостей - кэшируем на неделю
     path('person/<slug:slug>/', views.star_detail, name='star_detail'),
+
+    # Страницы с формами - не кэшируем
     path('about/', views.about, name='about'),
+    path('add/', views.add_star, name='add_star'),
+    path('search/', views.search, name='search'),
+
+    # Каталожные страницы - кэшируем на день
     path('country/<slug:slug>/', views.stars_by_country, name='stars_by_country'),
     path('industry/<slug:slug>/', views.stars_by_category, name='stars_by_category'),
-    path('add/', views.add_star, name='add_star'),
+    path('tag/<slug:tag_slug>/', views.tag, name='tag'),
 
-    # Новые URL
-    path('search/', views.search, name='search'),
+    # Календарные страницы - кэшируем на день
     path('birthday/<int:month>-<int:day>/', views.birthday, name='birthday'),
     path('dates/', views.dates, name='dates'),
+
+    # Статические страницы - кэшируем долго
     path('celebrities/', views.celebrities, name='celebrities'),
-    path('tag/<slug:tag_slug>/', views.tag, name='tag'),
     path('rules/', views.rules, name='rules'),
     path('names/', views.names, name='names'),
     path('names/<str:letter>/', views.names_letter, name='names_letter'),
